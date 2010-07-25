828c729de3d9068c02f054020bb70152a7b2e35e
Prakash Mohan <prakash.mohan@kdemail.net>
2010-07-26 04:29:24 +0530

    Adding a check to the file format specified

--- postit.sh
+++ postit.sh
@@ -37,5 +37,40 @@ if [[ ! -f "$1" ]] ; then
     echo "You will get the pastebin.com url as the output!"
     exit
 fi
+test=""
+#echo $test
+ if [[ ! -z "$2" ]] ; then
+    formats=(abap actionscript actionscript3 ada apache \
+        applescript apt_sources asm asp autoit avisynth \
+        bash basic4gl bibtex blitzbasic bnf boo bf c \
+        c_mac cill csharp cpp caddcl cadlisp cfdg \
+        klonec klonecpp cmake cobol cfm css d dcs \
+        delphi dff div dos dot eiffel email erlang \
+        fo fortran freebasic gml genero gettext groovy \
+        haskell hq9plus html4strict idl ini inno intercal \
+        io java java5 javascript kixtart latex lsl2 \
+        lisp locobasic lolcode lotusformulas lotusscript \
+        lscript lua m68k make matlab matlab mirc \
+        modula3 mpasm mxml mysql text nsis oberon2 objc \
+        ocaml-brief ocaml glsl oobas oracle11 oracle8 \
+        pascal pawn per perl php php-brief pic16 \
+        pixelbender plsql povray powershell progress prolog \
+        properties providex python qbasic rails rebol reg \
+        robots ruby gnuplot sas scala scheme scilab \
+        sdlbasic smalltalk smarty sql tsql tcl tcl \
+        teraterm thinbasic typoscript unreal vbnet verilog \
+        vhdl vim visualprolog vb visualfoxpro whitespace \
+        whois winbatch xml xorg_conf xpp z80 \
+        )
+    for i in ${formats[*]} ; do
+        if [[ "$2" = $i ]] ; then
+            test="pass"
+        fi
+    done
+    if [[ -z "$test" ]]; then
+        echo "Invalid Format, enter again"
+        exit 1
+    fi
+ fi
 curl http://pastebin.com/api_public.php -d paste_format="$2" --data-urlencode paste_code@$1 -d paste_name="$1"
 echo
