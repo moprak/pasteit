7fa2d4118cefdaf16aa23755e946268d06024be6
Sivaramakrishnan <ssivark007@yahoo.co.in>
2010-07-30 00:29:04 +0530

    Massive overhaul of the script. Getopts used to provide options(format, author, expiry time, verbosity). Checks and error messages for format and expiry time. Suitable status displays at relevant points for the verbosity option. Slight modification in the last step, passing curl as an argument to an if statement. To be seen if that works out.

--- pasteit.sh
+++ pasteit.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 # Copyright (c) 2010, Prakash Mohan and Sivaramakrishnan Swaminathan
 # All rights reserved.
@@ -30,17 +30,47 @@
 # A shell interface upload files to pastebin.com
 # More information in the README file.
 # TODO BlogPost
+# Create a MAN page
+# Whats the difference b/w SH(dash), BASH and ZSH. Which is standard ?
 
-if [[ ! -f "$1" ]] ; then
-    echo "Usage: $0 <path-to-file>"
-    echo "Or,    $0 <path-to-file> <format> <paste-name> <expiry time (N, 10M, 1H, 1D, 1M)>"
-    echo "You will get the pastebin.com url as the output!"
-    exit
+PROG_NAME=$(basename $0)
+
+if [[ ! -f "$1"  ]] ; then
+    echo "Usage: $PROG_NAME <path-to-file>";
+    echo "Or,    $PROG_NAME <path-to-file> [ -f <format> -a <author> -x <expiry time (N, 10M, 1H, 1D, 1M)> -v ]";
+    echo "You will get the pastebin.com url as the output!";
+    exit 1 ;
 fi
 
-test=""
- if [[ ! -z "$2" ]] ; then
-    formats=(abap actionscript actionscript3 ada apache \
+file="$1"
+
+shift 1
+
+test_format="pass"
+test_expiry="pass"
+verbose=""
+
+# can be written without getopts, using case command
+while getopts f:a:x:v op
+do
+    case $op in
+        f) format=$OPTARG
+            test_format="";;
+        a) author=$OPTARG;;
+        x) expiry=$OPTARG
+            test_expiry="";;
+        v)verbose="true";;
+      \?) print -u2 "Usage: $0 <filename> [ -f format -a author -x expiry ]"
+           exit 2;;
+    esac
+done
+
+if [[ ! -z "$verbose" ]]; then
+    echo "Contents of file $file are accepted."
+fi
+
+# Testing for validity of format
+formats=(abap actionscript actionscript3 ada apache \
         applescript apt_sources asm asp autoit avisynth \
         bash basic4gl bibtex blitzbasic bnf boo bf c \
         c_mac cill csharp cpp caddcl cadlisp cfdg \
@@ -60,24 +90,49 @@ test=""
         sdlbasic smalltalk smarty sql tsql tcl tcl \
         teraterm thinbasic typoscript unreal vbnet verilog \
         vhdl vim visualprolog vb visualfoxpro whitespace \
-        whois winbatch xml xorg_conf xpp z80 \
-        )
-    for i in ${formats[*]} ; do
-        if [[ "$2" = $i ]] ; then
-            test="pass"
-        fi
-    done
-    if [[ -z "$test" ]]; then
-        echo "Invalid Format, enter again"
-        exit 1
-    fi
- fi
+        whois winbatch xml xorg_conf xpp z80);
+ 
+#if [[ ! -z $format ]] ; then
+for i in ${formats[*]} ; do
+    if [[ "$format" = "$i" ]] ; then
+#        echo "format pass";
+        test_format="pass";
+    fi;
+done
 
-expiry="N"
-
-if [[ ! -z $4 ]] ; then
-    expiry=$4
+if [[ -z $test_format ]] ; then
+    echo "Invalid format, enter again."
+    exit 3;
+elif [[ ! -z "$verbose" ]]; then
+    echo "Format accepted.";
 fi
 
-curl http://pastebin.com/api_public.php -d paste_format="$2" --data-urlencode paste_code@$1 --data-urlencode paste_name="$3" -d paste_expiry="$4"
-echo
+#Testing for validity of expiry
+expirys=(N 10M 1H 1D 1M);
+#if [[ ! -z "$expiry" ]] ; then
+for j in ${expirys[*]};do
+    if [[ "$expiry" = "$j" ]] ; then
+#        echo "expiry pass";
+        test_expiry="pass";
+    fi
+done
+
+if [[ -z $test_expiry ]] ; then
+    echo "Invalid expiry time, enter again."
+    exit 4;
+elif [[ ! -z "$verbose" ]]; then
+    echo "Expiry time accepted.";
+fi
+
+#sending the contents to pastebin
+if [[ ! -z "$verbose" ]]; then
+    echo "Sending the contents to pastebin.com ...";
+fi
+
+if [[ `curl http://pastebin.com/api_public.php -d \
+    paste_format=$format --data-urlencode paste_code@$file \
+    --data-urlencode paste_name=$author -d paste_expiry=$expiry` ]]; then
+    if [[ ! -z "$verbose" ]]; then
+        echo "There's the link! :-)";
+    fi
+fi
